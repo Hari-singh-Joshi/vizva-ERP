1c376c7b8c20dbc705942cb1aca00cadb5845bfd
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-09-18 00:51:19 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit  # exit if any command fails
+set -o pipefail
+set -o nounset
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+
+
+# Start Gunicorn
+gunicorn vizva.wsgi:application --bind 0.0.0.0:$PORT
