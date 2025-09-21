980454d41425da89c2da4a194275a9476bb70ef8
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-09-21 21:59:48 +0530

    Update settings.py

--- vizva/settings.py
+++ vizva/settings.py
@@ -206,7 +206,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
     "theme": "default",
-    "dark_mode_theme": "darkly",
+    "dark_mode_theme": None,
     "button_classes": {
         "primary": "btn-primary",
         "secondary": "btn-secondary",
