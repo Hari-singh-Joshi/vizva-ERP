a15d09efc3202c56af7f41de447bf039547f6537
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-11-20 05:51:46 -0500

    Add timer path to URLs

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('case/<int:pk>/edit/', views.case_edit, name='case-edit'),
     path('po-entries/', views.po_entry_list, name='ph_list'),
     path('candidates/', views.candidate_list, name='candidate_list'),
+     path('timer/', views.timer, name="timer"),
 ]
